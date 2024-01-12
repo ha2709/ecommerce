acc42698e3855663db858f55b5eee4ab326f6832
“Henry <ngpha@yahoo.com>
2024-01-12 18:29:43 +0700

    create src folder, readme, docker

---
+++ src/models/user.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String, Float, ForeignKey, Table, Boolean
+from sqlalchemy.orm import relationship
+from database import Base
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+
+    orders = relationship("Order", back_populates="owner")
+
+  
